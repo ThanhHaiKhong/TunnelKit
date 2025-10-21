b87c8b3b95407156a73b708d7f6f5ad0cd1b2aac
ThanhHaiKhong <khongthanhhai@gmail.com>
2025-10-21 16:18:37 +0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,164 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TunnelKit",
+    platforms: [
+        .iOS(.v15), .macOS(.v12), .tvOS(.v17)
+    ],
+    products: [
+		.singleTargetLibrary("TunnelKit"),
+		.singleTargetLibrary("TunnelKitOpenVPN"),
+		.singleTargetLibrary("TunnelKitOpenVPNAppExtension"),
+		.singleTargetLibrary("TunnelKitLZO")
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.0"),
+        .package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "3.2.105"),
+    ],
+    targets: [
+        .target(
+            name: "TunnelKit",
+            dependencies: [
+                "TunnelKitCore",
+                "TunnelKitManager"
+            ]
+        ),
+        .target(
+            name: "TunnelKitCore",
+            dependencies: [
+                "__TunnelKitUtils",
+                "CTunnelKitCore",
+                "SwiftyBeaver"
+            ]
+		),
+        .target(
+            name: "TunnelKitManager",
+            dependencies: [
+                "SwiftyBeaver"
+            ]
+		),
+        .target(
+            name: "TunnelKitAppExtension",
+            dependencies: [
+                "TunnelKitCore"
+            ]
+		),
+        .target(
+            name: "TunnelKitOpenVPN",
+            dependencies: [
+                "TunnelKitOpenVPNCore",
+                "TunnelKitOpenVPNManager"
+            ]
+		),
+        .target(
+            name: "TunnelKitOpenVPNCore",
+            dependencies: [
+                "TunnelKitCore",
+                "CTunnelKitOpenVPNCore",
+                "CTunnelKitOpenVPNProtocol" // FIXME: remove dependency on TLSBox
+            ]
+		),
+        .target(
+            name: "TunnelKitOpenVPNManager",
+            dependencies: [
+                "TunnelKitManager",
+                "TunnelKitOpenVPNCore"
+            ]
+		),
+        .target(
+            name: "TunnelKitOpenVPNProtocol",
+            dependencies: [
+                "TunnelKitOpenVPNCore",
+                "CTunnelKitOpenVPNProtocol"
+            ]
+		),
+        .target(
+            name: "TunnelKitOpenVPNAppExtension",
+            dependencies: [
+                "TunnelKitAppExtension",
+                "TunnelKitOpenVPNCore",
+                "TunnelKitOpenVPNManager",
+                "TunnelKitOpenVPNProtocol"
+            ]
+		),
+        .target(
+            name: "TunnelKitLZO",
+            dependencies: [],
+            exclude: [
+                "lib/COPYING",
+                "lib/Makefile",
+                "lib/README.LZO",
+                "lib/testmini.c"
+            ]
+		),
+        .target(
+            name: "CTunnelKitCore",
+            dependencies: [
+				
+			]
+		),
+        .target(
+            name: "CTunnelKitOpenVPNCore",
+            dependencies: [
+				
+			]
+		),
+        .target(
+            name: "CTunnelKitOpenVPNProtocol",
+            dependencies: [
+                "CTunnelKitCore",
+                "CTunnelKitOpenVPNCore",
+                "openssl-apple"
+            ]
+		),
+        .target(
+            name: "__TunnelKitUtils",
+            dependencies: [
+				
+			]
+		),
+        .testTarget(
+            name: "TunnelKitCoreTests",
+            dependencies: [
+                "TunnelKitCore"
+            ],
+            exclude: [
+                "RandomTests.swift",
+                "RawPerformanceTests.swift",
+                "RoutingTests.swift"
+            ]
+		),
+        .testTarget(
+            name: "TunnelKitOpenVPNTests",
+            dependencies: [
+                "TunnelKitOpenVPNCore",
+                "TunnelKitOpenVPNAppExtension",
+                "TunnelKitLZO"
+            ],
+            exclude: [
+                "DataPathPerformanceTests.swift",
+                "EncryptionPerformanceTests.swift"
+            ],
+            resources: [
+                .process("Resources")
+            ]
+		),
+        .testTarget(
+            name: "TunnelKitLZOTests",
+            dependencies: [
+                "TunnelKitCore",
+                "TunnelKitLZO"
+            ]
+		)
+    ]
+)
+
+extension Product {
+	static func singleTargetLibrary(_ name: String) -> Product {
+		.library(name: name, targets: [name])
+	}
+}
+
